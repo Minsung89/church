0fc3146ee0efc15466b4ec09d1c5a8dedb05c63f
MinSung89 <minsung.koo@systemk.kr>
2019-09-18 11:56:34 +0900

    DB 생성

---
+++ app/src/main/java/com/ydn/church/Database/ChurchDB.java
@@ -0,0 +1,37 @@
+package com.ydn.church.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class ChurchDB {
+    Context context;
+    //SQLite DB
+    SQLiteDatabase db;
+
+
+    //SQLite(DB 이름)
+    String DATABASE_NAME = "CHURCH";
+
+
+    ChurchDB(Context context){
+        this.context = context;
+    }
+    //데이터베이스 접속 또는 만들기
+    public void connectDatabase(){
+        try {
+            db = context.openOrCreateDatabase(DATABASE_NAME, context.MODE_PRIVATE, null);
+            Log.i("Database", "OK");
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.i("Database", "NO");
+        }
+    }
+
+    //데이터베이스 테이블 만들기
+    private void createTable(){
+        db.execSQL("CREATE TABLE IF NOT EXISTS MALSSEUM(TITLE VARCHAR2(20) NOT NULL, PAGE VARCHAR2(20) NOT NULL, SECTION VARCHAR2(20) NOT NULL);");
+
+    }
+
+}
